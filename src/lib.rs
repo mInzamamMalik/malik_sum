814c6407fce3cb20265f2a2a3a9b96bd25ca839c
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2019-10-09 05:05:09 +0500

    four math functions added

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
-pub mod sum {
-    pub fn add() {
-        println!("adding num");
+use std::ops::Add;
+
+pub fn add<T>(num1: u32, num2: u32) -> u32 {
+    num1 + num2
+}
+
+
+pub fn subtract(num1: u32, num2: u32) -> u32 {
+    num1 - num2
+}
+
+
+pub fn multiply(num1: u32, num2: u32) -> u32 {
+    num1 * num2
+}
+
+
+pub fn divide(num1: u32, num2: u32) -> u32 {
+    if (num2 == 0) {
+        println!("can not divide with zero");
+        0
+    } else {
+        num1 / num2
     }
 }
-pub use sum::add;
+
+
+pub fn power(num1: u32, num2: u32) -> u32 {
+    num1.pow(num2)
+}
